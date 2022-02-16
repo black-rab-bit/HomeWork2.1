c7a4087ce021e3210f3de52f723e92d0d48086f0
TesterSinet <tanya@sinet.team>
2022-02-17 00:17:48 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketAmount = 175; // Переменная для хранения стоимости билета
+        int bonusConst = 20;    // Переменная для хранения хранения количества рублей для одной бонусной милли
+
+        int passengersBonus = ticketAmount/bonusConst; // Рассчитываем количество бонусных миль
+        System.out.println("Вам начислено "+ passengersBonus+ " бонусных миль"); //  Ответ сохраняем в новую переменную
+                                                                                 // и выводите на экран
+
+    }
+}
